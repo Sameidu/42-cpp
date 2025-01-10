e6b4b590aab9959a75c910afd41c2f21d91522c3
Sara Meixoeiro durante <smeixoei@c2r4s4.42madrid.com>
2025-01-10 14:25:20 +0100

    finish ex02

--- cpp00/ex02/Account.cpp
+++ cpp00/ex02/Account.cpp
@@ -34,17 +34,17 @@ Account::~Account () {
 
 void	Account::displayAccountsInfos( void ) {
 	_displayTimestamp();
-	std::cout << "accounts:" << Account::_nbAccounts;
-	std::cout << ";total:" << Account::_totalAmount;
-	std::cout << ";deposit:" << Account::_totalNbDeposits;
-	std::cout << ";withdrawals:" << Account::_totalNbWithdrawals << std::endl;
+	std::cout << "accounts:" << getNbAccounts();
+	std::cout << ";total:" << getTotalAmount();
+	std::cout << ";deposits:" << getNbDeposits();
+	std::cout << ";withdrawals:" << getNbWithdrawals() << std::endl;
 };
 
 void	Account::makeDeposit ( int deposit ) {
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";p_amount:" << _amount;
-	std::cout << ";deposits:" << deposit;
+	std::cout << ";deposit:" << deposit;
 	_amount += deposit;
 	std::cout << ";amount:" << _amount;
 	_nbDeposits++;
@@ -57,19 +57,17 @@ bool	Account::makeWithdrawal ( int withdrawal ) {
 	_displayTimestamp();
 	std::cout << "index:" << _accountIndex;
 	std::cout << ";p_amount:" << _amount;
-	std::cout << ";withdrawal:" << withdrawal;
-	_amount = withdrawal;
-	std::cout << ";amount:" << _amount;
-	_nbWithdrawals++;
-	std::cout << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
-	Account::_totalAmount -= withdrawal;
-	Account::_totalNbWithdrawals++;
-	return 1;
-};
-
-int		Account::checkAmount( void ) const {
-	_displayTimestamp();
-	std::cout << "checkAmount" << std::endl;
+	if (withdrawal > _amount)
+		std::cout << ";withdrawal:refused" << std::endl;
+	else {
+		std::cout << ";withdrawal:" << withdrawal;
+		_amount -= withdrawal;
+		std::cout << ";amount:" << _amount;
+		_nbWithdrawals++;
+		std::cout << ";nb_withdrawals:" << _nbWithdrawals << std::endl;
+		Account::_totalAmount -= withdrawal;
+		Account::_totalNbWithdrawals++;
+	}
 	return 1;
 };
 
@@ -84,6 +82,10 @@ void	Account::displayStatus( void ) const {
 void	Account::_displayTimestamp( void ) {
 	std::cout << "[19920104_091532] ";
 }
+
+int		Account::checkAmount( void ) const {
+	return 1;
+};
 
 int	Account::getNbAccounts( void ) {
 	return Account::_nbAccounts;
