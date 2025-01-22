c46f69c7d68c1ca2b14c15b6d3156cd5a8a16204
Sara Meixoeiro durante <smeixoei@c3r14s2.42madrid.com>
2025-01-22 19:57:36 +0100

    create files cpp02/ex03

---
+++ cpp04/ex03/AMateria.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef AMATERIA_HPP
+#define AMATERIA_HPP
+
+#include <string>
+#include <iomanip>
+#include <iostream>
+
+class AMateria {
+	protected:
+		std::string _type;
+
+	public:
+		AMateria(std::string const & type);
+		~AMateria ();
+		AMateria (const AMateria &other);
+
+		AMateria &operator= (const AMateria &other);
+
+		std::string const & getType() const; //Returns the materia type
+		virtual AMateria* clone() const = 0;
+		virtual void use(ICharacter& target);
+};
+
+#endif
